bace1683495b7af1c548abfdf0a981bdf4a8375b
Vinson <1760078936@qq.com>
2020-04-17 21:34:21 +0800

    添加跳跃游戏

--- src/main/java/com/vinson/algorithm/game/JumpGame.java
+++ src/main/java/com/vinson/algorithm/game/JumpGame.java
@@ -4,7 +4,29 @@ package com.vinson.algorithm.game;
  * @Name: JumpGame
  * @Date: 2020/4/17 20:37
  * @User: Vinson
- * @desc:
+ * @desc: https://leetcode-cn.com/problems/jump-game/
  */
 public class JumpGame {
+
+    public boolean canJump(int[] nums) {
+        int length = nums.length;
+        int start = 0;
+        for(int i = 0; i < length; i++){
+            if(start < i){
+                return false;
+            }
+            start = Math.max(start, i + nums[i]);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,3,1,1,4};
+        JumpGame jumpGame = new JumpGame();
+        System.out.println(jumpGame.canJump(arr));
+    }
+
+
+
+
 }
--- src/main/java/com/vinson/algorithm/sort/QuickSort.java
+++ src/main/java/com/vinson/algorithm/sort/QuickSort.java
@@ -1,5 +1,8 @@
 package com.vinson.algorithm.sort;
 
+import com.alibaba.fastjson.JSON;
+import com.vinson.util.CommonUtil;
+
 /**
  * @Name: QuickSort
  * @Date: 2019/12/22 17:39
@@ -7,4 +10,21 @@ package com.vinson.algorithm.sort;
  * @desc:
  */
 public class QuickSort {
+
+
+    public Integer[] sort(Integer arr){
+
+        return new Integer[]{};
+    }
+
+
+
+    public static void main(String[] args) {
+        QuickSort test = new QuickSort();
+        Integer[] ramdomArr = CommonUtil.getRamdomArr(20, 1, 30);
+        System.out.println("原始数组："+JSON.toJSONString(ramdomArr));
+//        System.out.println("排序后的数组：" + JSON.toJSONString(test.sort(ramdomArr)));
+    }
+
+
 }
